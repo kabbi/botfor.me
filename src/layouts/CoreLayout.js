0760344e98a103a70fd00a2ce10ed44a93123631
Vadim Boltach <vadik.boltach@gmail.com>
2016-01-12 17:40:32 +0300

    MDL script upgrading fix.

--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -4,17 +4,20 @@ import 'material-design-lite/material.min.css';
 import 'material-design-lite/material.min.js';
 
 import HeaderView from 'views/HeaderView';
+import MaterialLiteUpgrade from 'components/MaterialLiteUpgrade';
 
 function CoreLayout({ children }) {
   return (
-    <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <HeaderView/>
-      <main className="mdl-layout__content">
-        <div className="page-content">
-          {children}
-        </div>
-      </main>
-    </div>
+    <MaterialLiteUpgrade>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <HeaderView/>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            {children}
+          </div>
+        </main>
+      </div>
+    </MaterialLiteUpgrade>
   );
 }
 
