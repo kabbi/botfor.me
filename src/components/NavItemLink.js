42410a03b7837bcbedfda0093186ed0395b1818a
Vadim Boltach <vadik.boltach@gmail.com>
2016-01-15 01:40:01 +0300

    Removed console.logs

--- src/components/NavItemLink.js
+++ src/components/NavItemLink.js
@@ -1,11 +1,7 @@
 import { NavItem } from 'react-bootstrap';
 
-import { routeActions } from 'redux-simple-router';
-
 const handleNavItemClick = (href, e) => {
   e.preventDefault();
-  console.log('href', routeActions);
-  // routeActions.push(href);
 };
 
 const NavItemLink = (props) => (
